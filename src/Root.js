1b8b061ca4c2cfa4d1898ca630db618bc7a8ade2
woobin <wb1762@gmail.com>
2023-08-17 08:34:33 +0900

    first commit

---
+++ src/Root.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function Root() {
+  const [counter, setNumber] = useState(0);
+  const countUp = () => {
+    setNumber(counter + 1);
+  }
+
+  const countDown = () => {
+    setNumber(counter - 1);
+  }
+
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={countUp}>up</button>
+      <button onClick={countDown}>down</button>
+    </div>
+  );
+}
+ 
+
+export default Root;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Root  from './Root';
+import reportWebVitals from './reportWebVitals';
+
+ 
+ReactDOM.render(  <Root />,
+  document.getElementById('root')
+
+);
+
+
+reportWebVitals();
