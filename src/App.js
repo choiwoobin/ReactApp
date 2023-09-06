c6ab000ae07324991eb26bbe8d4ac77eafb1636b
woobin <wb1762@gmail.com>
2023-09-06 15:20:19 +0900

    lifeCycle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponent from './MyComponent'
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import './App.css';
+import LifeCycleSample from './LifeCycleSample';
 
-class App extends Component {
-  render(){
-    
-    return (
-      <IterationSample/>
-    );
-  }
+//랜덤 색상 생성 
+
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  //16777215를 hex로 표현하면 ffffff가 되므로 000000 ~ ffffff 값 반환
 }
 
+class App extends Component {
+  state ={
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
+    );
+  }
+
+}
 export default App;
